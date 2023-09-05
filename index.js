d0c53fb4e439aba7464dc723c502b3ac699c0919
mwang840 <maxwang@udel.edu>
2023-09-05 14:52:22 -0400

    exercise three done

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    });
+$(document).ready(function(){
+    console.log("Ready!");
+
+    $('#go').on("click", function(event){
+        var fullName = $("#fname").val() + " " + $("#lname").val();
+        $("#jumbotron").html(fullName);
+        return false;
+    })
+})
+
